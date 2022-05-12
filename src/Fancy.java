40281171eb0edb96d9dd1e83da77bf2365dc0efc
mahamud <shasan181005@bscse.uiu.ac.bd>
2022-05-12 13:02:08 +0600

    starting commit

---
+++ src/Fancy.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Fancy {
+    public static void main(String[] args) {
+       int a;
+       int b;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n);
+
+    }
+}
