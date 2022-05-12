955fe91d6ca7911e94ff7c7660092ea0d8ea5348
TANJIM <rtanjim182016@bscse.uiu.ac.bd>
2022-05-12 16:01:34 +0600

    new code added on May 10

--- src/Fancy.java
+++ src/Fancy.java
@@ -4,6 +4,7 @@ public class Fancy {
     public static void main(String[] args) {
        int a;
        int b;
+       int tanjim;
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         System.out.println(n);
